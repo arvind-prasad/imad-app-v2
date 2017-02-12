78f57ba7cff7846de791cf73f7550f065c627c00
arvind-prasad <arvindprasad1289@gmail.com>
2017-02-12 09:39:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,18 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/article-one', function (req, res) {
+  res.sen("Article one requested");
+});
+
+app.get('/article-two', function (req, res) {
+  res.sen("Article two requested");
+});
+
+app.get('/article-three', function (req, res) {
+  res.sen("Article three requested");
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
